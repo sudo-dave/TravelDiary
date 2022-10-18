36cba79e3067e8f27293b5e4b5ae55678d778237
David M <david.macias.0101@gmail.com>
2022-10-18 12:47:46 -0700

    Init vite.js + react w/ typescript + tailwind css

---
+++ front/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="App">
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+      Install & Setup Vite + React + Typescript + Tailwind CSS 3
+      </h1>
+    </div>
+    </div>
+  )
+}
+
+export default App
